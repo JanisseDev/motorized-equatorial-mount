4a3131f36f6787bff125c9fe35177c9174c5f0ba
JanisseDev <64750827+JanisseDev@users.noreply.github.com>
2023-08-22 15:39:51 +0200

    Clean main.c

---
+++ server/server.h
@@ -0,0 +1,54 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <string.h>
+
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+
+#include "dhcpserver.h"
+#include "dnsserver.h"
+
+#define TCP_PORT 80
+#define DEBUG_printf printf
+#define POLL_TIME_S 5
+#define HTTP_GET "GET"
+#define HTTP_RESPONSE_HEADERS "HTTP/1.1 %d OK\nContent-Length: %d\nContent-Type: text/html; charset=utf-8\nConnection: close\n\n"
+#define LED_TEST_BODY "<html><body><h1>Hello from Piiico W.</h1><p>Led is %s</p><p><a href=\"?led=%d\">Turn led %s</a></body></html>"
+#define LED_PARAM "led=%d"
+#define LED_TEST "/ledtest"
+#define LED_GPIO 0
+#define HTTP_RESPONSE_REDIRECT "HTTP/1.1 302 Redirect\nLocation: http://%s" LED_TEST "\n\n"
+
+typedef struct TCP_SERVER_T_ {
+    struct tcp_pcb *server_pcb;
+    bool complete;
+    ip_addr_t gw;
+    async_context_t *context;
+} TCP_SERVER_T;
+
+typedef struct TCP_CONNECT_STATE_T_ {
+    struct tcp_pcb *pcb;
+    int sent_len;
+    char headers[128];
+    char result[256];
+    int header_len;
+    int result_len;
+    ip_addr_t *gw;
+} TCP_CONNECT_STATE_T;
+
+extern TCP_SERVER_T *state;
+extern dhcp_server_t dhcp_server;
+extern dns_server_t dns_server;
+
+err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err);
+void key_pressed_worker_func(async_context_t *context, async_when_pending_worker_t *worker);
+void key_pressed_func(void *param);
+
+int serverInit();
+void serverDeinit();
+
+#endif
