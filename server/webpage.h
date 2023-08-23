89ca46b4fade59d7c8eaef6b9912ae48394757cf
JanisseDev <64750827+JanisseDev@users.noreply.github.com>
2023-08-23 09:54:28 +0200

    Remove test

--- server/webpage.h
+++ server/webpage.h
@@ -4,7 +4,7 @@
 #include "pico/stdlib.h"
 
 #define HTTP_RESPONSE_HEADERS "HTTP/1.1 %d OK\nContent-Length: %d\nContent-Type: text/html; charset=utf-8\nConnection: close\n\n"
-#define LED_TEST_BODY "<html><body><h1>Hello from Piiico W.</h1><p>Led is %s</p><p><a href=\"?led=%d\">Turn led %s</a></body></html>"
+#define LED_TEST_BODY "<html><body><h1>Hello from Pico W.</h1><p>Led is %s</p><p><a href=\"?led=%d\">Turn led %s</a></body></html>"
 #define LED_PARAM "led=%d"
 #define LED_TEST "/ledtest"
 #define LED_GPIO 0
