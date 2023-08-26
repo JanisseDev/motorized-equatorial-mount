e031dfe4816cd2d627b3a678b451d9dde937b131
JanisseDev <64750827+JanisseDev@users.noreply.github.com>
2023-08-27 00:14:28 +0200

    Fix last commit build issue

--- ControlVariables.h
+++ ControlVariables.h
@@ -2,6 +2,6 @@
 
 #include "pico/stdlib.h"
 
-// /!\ This is a big shortcut! Expect some trouble you use more complex variables, or if both cores edit these. (This can be solved with a lock/mutex system) /!\ 
+// /!\ This is a big shortcut! Expect some trouble you use more complex variables, or if both cores edit these. (This can be solved with a lock/mutex system) /!\ //
 extern volatile bool blinking;
 extern volatile uint32_t blinkSpeed;
